bf215609f34df995d373e94093b49c835387624c
Rapha-Y <raph.yuki@gmail.com>
2020-05-13 19:59:51 -0300

    added sum function (number strong typing)

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("AAAAAAaa!! I'm screaming");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 13;
+var number2 = 3.14;
+var result = add(number1, number2);
+console.log(result);
