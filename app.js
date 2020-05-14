a24d62accb3a3b8a968ed16221447c1c477a9533
Rapha-Y <raph.yuki@gmail.com>
2020-05-14 17:07:42 -0300

    added enum

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Jolyne Cujoh',
     age: 18,
-    hobbies: ['Sewing', 'Punching']
+    hobbies: ['Sewing', 'Punching'],
+    role: Role.ADMIN
 };
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
+}
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
 }
--- app.ts
+++ app.ts
@@ -1,17 +1,18 @@
-const person: {
-    name:string,
-    age:number,
-    hobbies:string[],
-    role:[number, string]
-} = {
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
     name: 'Jolyne Cujoh',
     age: 18,
     hobbies: ['Sewing', 'Punching'],
-    role: [2, 'Prisoner']
+    role: Role.ADMIN
 };
 
 console.log(person.name);
 
 for(const hobby of person.hobbies) {
     console.log(hobby);
+}
+
+if(person.role === Role.AUTHOR) {
+    console.log("is author");
 }
