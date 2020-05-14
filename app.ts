054290c56e32ae153e6e559d4bbe3baee8269431
Rapha-Y <raph.yuki@gmail.com>
2020-05-14 17:27:37 -0300

    added output type in combine function (literal types)

--- app.ts
+++ app.ts
@@ -1,15 +1,22 @@
-function combine(input1:number|string, input2:number|string) {
+function combine(
+    input1:number|string, 
+    input2:number|string, 
+    resultConversion:'as-number'|'as-text'
+) {
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
 
-const combinedAges = combine(21, 25);
+const combinedAges = combine(21, 25, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Maxine', 'Chloe');
+const combinedStringAges = combine('21', '25', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Maxine', 'Chloe', 'as-text');
 console.log(combinedNames);
