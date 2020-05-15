a1e3a876939ced0e7f206478fb550b1bb03a5b6d
Rapha-Y <raph.yuki@gmail.com>
2020-05-15 18:35:04 -0300

    added compiler configuration (exclusion/inclusion)

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-var userInput;
-var userName;
-userInput = 7;
-userInput = 'May';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw {
-        message: message,
-        errorCode: code
-    };
-}
-generateError('An error occurred!', 500);
+"use strict";
+var userName = 'Maxine';
+console.log(userName);
